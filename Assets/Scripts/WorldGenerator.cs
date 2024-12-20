22e7f5fdbce7df06aa8be8a8feefcdad79d7b031
Noé Mouchel <noe.mouchel@gmail.com>
2024-12-20 10:43:03 +0100

    cleaning code
    Co-authored-by: Rémi GINER <remi.giner@hotmail.fr>

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -53,42 +53,26 @@ public class WorldGenerator : ScriptableObject
 
     public void UpdateCells(ChunkLoadComponent chunkLoader, CellsData cellsData, Transform cellContainer)
     {
-        if (chunkLoader.OldPosition == chunkLoader.transform.position)
-            return;
+        Vector2Int loaderPosition   = FromWorldSpaceToGridSpace(chunkLoader.transform.position);
+        Vector2Int oldPosition2D    = FromWorldSpaceToGridSpace(chunkLoader.OldPosition);
+        Vector2Int deltaPosition    = loaderPosition - oldPosition2D;
+        Vector2Int extent           = Vector2Int.one * chunkLoader.VirtualDistance;
 
-        Vector2Int loaderPosition = FromWorldSpaceToGridSpace(chunkLoader.transform.position);
-        Vector2Int oldPosition2D = FromWorldSpaceToGridSpace(chunkLoader.OldPosition);
-
-        Vector2Int deltaPosition = loaderPosition - oldPosition2D;
-
+        Rect newRect = new Rect(loaderPosition - extent, extent*2);
+        
         float sqrMovement = deltaPosition.sqrMagnitude;
 
-        Vector2Int extent = Vector2Int.one * chunkLoader.VirtualDistance;
-        Rect newRect = new Rect(loaderPosition - extent, extent*2);
+        //  Clearing part
+        //  -------------
 
         //  if the distance traveled is higher than the virtual distance, clear all datas
         if (sqrMovement > chunkLoader.SquaredVirtualDistance * 4)
-        {
-            cellsData.ClearAll();
-        }
+            Wipe(cellsData);
         else
-        {
-            Rect oldRect = new Rect(oldPosition2D - extent, extent*2);
-            Rect[] areaToClear = oldRect.Subtract(newRect);
+            SweepBehind(cellsData, new Rect(oldPosition2D - extent, extent * 2), newRect);
 
-            foreach (Rect rect in areaToClear)
-            {
-                rect.DrawRect(Color.red, 1);
-                Vector2Int pos = Vector2Int.zero;
-                for (pos.x = (int)rect.xMin; pos.x < (int)rect.xMax; pos.x++)
-                {
-                    for (pos.y = (int)rect.yMin; pos.y < (int)rect.yMax; pos.y++)
-                    {
-                        cellsData.ClearAt(pos);
-                    }
-                }
-            }
-        }
+        //  Loading part
+        //  ------------
 
         Vector2Int currPosition = Vector2Int.zero;
         for (currPosition.x = (int)newRect.xMin; currPosition.x < (int)newRect.xMax; currPosition.x++)
@@ -100,40 +84,60 @@ public class WorldGenerator : ScriptableObject
 
                 //  Calculate square distance between current coordinate and chunk loader
                 int DistSqr = currPosition.SqrdDistance(loaderPosition);
-
                 if (DistSqr > chunkLoader.SquaredVirtualDistance)
                     continue;
 
-                //  The cell is currently rendered
                 if (cellsData.IsCellOfState(currPosition, CellsData.CellState.RENDERED))
                 {
+                    //  The cell is currently rendered
                     //  Should it be unloaded
                     if (DistSqr > chunkLoader.SquaredUnloadDistance)
-                    {
                         UnloadRenderedCell(cellsData.RenderedCells, currPosition);
-                    }
+                    continue;
                 }
-
-                //  The cell is supposed to be rendered but is not rendered yet
-                else if (cellsData.IsCellOfState(currPosition, CellsData.CellState.VIRTUAL))
+                
+                if (cellsData.IsCellOfState(currPosition, CellsData.CellState.VIRTUAL))
                 {
+                    //  The cell is supposed to be rendered but is not rendered yet
                     //  Should we render this cell ?
                     if (DistSqr <= chunkLoader.SquaredRenderDistance)
                         CreateRenderedCell(cellsData.RenderedCells, cellContainer, currPosition);
-                }
 
+                    continue;
+                }
                 //  The cell has not been registered in any state, let's check if it should be filled depending on a noise
-                else if (GetNoiseValueFromChunkSpace(currPosition.x, currPosition.y) >= 1f - spawnRate)
+                if (GetNoiseValueFromChunkSpace(currPosition.x, currPosition.y) >= 1f - spawnRate) 
                 {
                     CreateVirtualCell(cellsData.VirtualCells, currPosition);
+                    continue;
                 }
 
                 //  Mark this cell as empty as it has no state yet and was not filled by the noise
-                else
-                {
-                    cellsData.EmptyCells.Add(currPosition);
-                }
+                cellsData.EmptyCells.Add(currPosition);
             }
+        }
+    }
+
+    private static void Wipe(CellsData cellsData)
+    {
+        cellsData.ClearAll();
+    }
+
+    
+    //  Sweep all cells that were in the previous area but not in the new area
+    private static void SweepBehind(CellsData cellsData, Rect previousArea, Rect newArea, bool drawDebug = false)
+    {
+        Rect[] areaToClear = previousArea.Subtract(newArea);
+
+        foreach (Rect rect in areaToClear)
+        {
+            if (drawDebug)
+                rect.DrawRect(Color.red, 1);
+            
+            Vector2Int pos = Vector2Int.zero;
+            for (pos.x = (int)rect.xMin; pos.x < (int)rect.xMax; pos.x++)
+                for (pos.y = (int)rect.yMin; pos.y < (int)rect.yMax; pos.y++)
+                    cellsData.ClearAt(pos);
         }
     }
 
